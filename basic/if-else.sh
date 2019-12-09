cc4d309030a32c1649e472cd4202c7697814faec
yuanweimin <ywm00@qq.com>
2019-12-09 20:24:03 +0800

    update if-else example

--- basic/if-else.sh
+++ basic/if-else.sh
@@ -4,7 +4,7 @@
 
 read -p "please enter your file name: " file
 
-ls | grep $file &>/dev/null;
+ls $file &>/dev/null;
 if [ $? -ne 0 ]; then
     echo "$file don't exist, crate it ..."
     echo "hello world" > $file
