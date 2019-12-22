374f0381a77232b72377f59db8ab232915340b5b
yuanweimin <ywm00@qq.com>
2019-12-22 16:10:12 +0800

    update kill process

--- example/kill_process.sh
+++ example/kill_process.sh
@@ -3,11 +3,13 @@
 # kill process pid
 
 process="your process name"
-pid=$(ps -aux|grep $process|grep -v grep|awk '{print $2}')
+pids=$(ps aux|grep $process|grep -v grep|awk '{print $2}')
 
-kill $pid
-if [[ $? -eq 0 ]]; then
-    echo "$process pid=$pid is killed !"
-else
-    echo "process is not running !"
-fi
+for pid in $pids; do
+    kill $pids
+    if [[ $? -eq 0 ]]; then
+        echo "$process pid=$pid is killed !"
+    else
+        echo "process is not running !"
+    fi
+done
